ee6001af185919e95a39c68ec92802e3f815d2ec
[MelaniePorley] <melanie.porley@gmail.com>
2023-11-01 15:09:44 -0300

    *

--- scripts/app.js
+++ scripts/app.js
@@ -84,8 +84,8 @@ btnPost.addEventListener('click', ()=> {
         lastname: inputPostApellido.value
 };
  postMethod(object);
- AddNewPerson(newObject);
-})
+ AddNewPerson(Object);
+});
 
 function AddNewPerson(data){
 const add = 
